6c58e85ccdeb886bdca579106795108843499dda
Ian McKenzie <imckenzie777@gmail.com>
2020-05-01 03:16:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,11 +9,12 @@ def monopoly_with_second_tier
   # complexity.
   railroads = {
     :railroads {
-      :pieces => '4'
+      :pieces => '4',
+      :rent_in_dollars = {}
     }
   }
    = []
-  rent_in_dollars = {}
+
   names = {}
 
 end
